6d885d4c2d9a276e30f8f2c4acee4d4e03330f76
itsZens <danimr1999@gmail.com>
2019-01-15 16:08:21 +0100

    Solved conflicts in serverDomino.js trying to load Lobby.html (could not get /js/Lobby.js) and changed method of the username form (Index.html) from get to post

--- serverDomino.js
+++ serverDomino.js
@@ -9,7 +9,7 @@ function iniciar() {
 		var sortida;
 		var pathname = url.parse(request.url).pathname;
 
-		if(pathname != '/favicon.ico' && pathname != '/serverDomino.js' && pathname != '/css/Index.css' && pathname != '/Images/LogoDomino.png'){
+		if(pathname != '/favicon.ico' && pathname != '/serverDomino.js' && pathname != '/css/Index.css' && pathname != '/Images/LogoDomino.png' && pathname != '/js/Lobby.js'){
 			console.log("Petició per a  " + pathname + " rebuda.");
 		}
 
@@ -81,6 +81,18 @@ function iniciar() {
 			fs.readFile('./Lobby.html', function (err, sortida) {
 				response.writeHead(200, {
 					'Content-Type': 'text/html'
+				});
+				response.write(sortida);
+				response.end();
+			});
+		} else if (pathname == '/js/Lobby.js'){
+			response.writeHead(200, {
+				"Content-Type": "text/js; charset=utf-8"
+			});
+
+			fs.readFile('./js/Lobby.js', function (err, sortida) {
+				response.writeHead(200, {
+					'Content-Type': 'text/js'
 				});
 				response.write(sortida);
 				response.end();
